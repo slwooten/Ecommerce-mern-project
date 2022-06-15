9a7a606fe5d8447a94ef555ab72c7a0bdadeb52e
Seth Wooten <slwooten14@gmail.com>
2022-06-15 18:31:48 -0400

    react-app-created

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href='/'>Ecommerce</a>
+      </header>
+      <main>
+        list products here
+      </main>
+    </div>
+  );
+}
+
+export default App;
